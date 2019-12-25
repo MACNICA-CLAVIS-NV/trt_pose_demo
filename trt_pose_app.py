161d2d0786873077b447e394a0841357b6c8ae87
tfuru-macnica <57699379+tfuru-macnica@users.noreply.github.com>
2019-12-25 14:45:13 +0900

    Add files via upload

--- trt_pose_app.py
+++ trt_pose_app.py
@@ -29,17 +29,16 @@ import sys
 import cv2
 from pose_capture import PoseCaptureModel
 from video_app_utils import PipelineWorker
-from video_app_utils import ContinuousVideoCapture
-from video_app_utils import IntervalCounter
+from video_app_utils import ContinuousVideoProcess
 import argparse
 
-WINDOW_TITLE = 'NVIDIA Pose Estimation Model Demo'
 WIDTH = 224
 HEIGHT = 224
 INPUT_RES = (WIDTH, HEIGHT)
 MODEL_WEIGHTS = 'resnet18_baseline_att_224x224_A_epoch_249.pth'
 OPTIMIZED_MODEL = 'resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
 TASK_DESC = 'human_pose.json'
+
 
 class ColorConvert(PipelineWorker):
     
@@ -50,6 +49,7 @@ class ColorConvert(PipelineWorker):
         orgFrame = srcData
         frame = cv2.cvtColor(orgFrame, cv2.COLOR_BGR2RGB)
         return (True, (frame, orgFrame))
+
         
 class Resize(PipelineWorker):
 
@@ -61,6 +61,7 @@ class Resize(PipelineWorker):
         frame = cv2.resize(frame, INPUT_RES, interpolation=cv2.INTER_NEAREST)
         return (True, (frame, orgFrame))
 
+
 class Preprocess(PipelineWorker):
     
     def __init__(self, qsize, source, model):
@@ -71,6 +72,7 @@ class Preprocess(PipelineWorker):
         frame, orgFrame = srcData
         frame = self.model.preprocess(frame)
         return (True, (frame, orgFrame))
+
         
 class Inference(PipelineWorker):
     
@@ -82,6 +84,7 @@ class Inference(PipelineWorker):
         frame, orgFrame = srcData
         cmap, paf = self.model.infer(frame)
         return (True, (cmap, paf, orgFrame))
+
         
 class Postprocess(PipelineWorker):
     
@@ -94,91 +97,29 @@ class Postprocess(PipelineWorker):
         self.model.postprocess(cmap, paf, orgFrame)
         return (True, orgFrame)
 
+
+class PoseEstimationProcess(ContinuousVideoProcess):
+
+    def __init__(self, args):
+        super().__init__(args)
+        model = PoseCaptureModel( \
+            WIDTH, HEIGHT, MODEL_WEIGHTS, OPTIMIZED_MODEL, TASK_DESC)
+        colorConv = ColorConvert(args.qsize, self.capture)
+        resize = Resize(args.qsize, colorConv)
+        preprocess = Preprocess(args.qsize, resize, model)    
+        inference = Inference(args.qsize, preprocess, model)  
+        postprocess = Postprocess(args.qsize, inference, model)
+  
+        
 def main():
     # Parse the command line parameters
-    parser = argparse.ArgumentParser(description='NVIDIA Pose Estimation Model Demo')
-    parser.add_argument('--camera', '-c', \
-        type=int, default=0, metavar='CAMERA_NUM', \
-        help='Camera number, use any negative integer for MIPI-CSI camera')
-    parser.add_argument('--width', \
-        type=int, default=800, metavar='WIDTH', \
-        help='Capture width')
-    parser.add_argument('--height', \
-        type=int, default=600, metavar='HEIGHT', \
-        help='Capture height')
-    parser.add_argument('--fps', \
-        type=int, default=20, metavar='FPS', \
-        help='Capture frame rate')
-    parser.add_argument('--qsize', \
-        type=int, default=2, metavar='QSIZE', \
-        help='Capture queue size')
-    parser.add_argument('--qinfo', \
-        action='store_true', \
-        help='If set, print queue status information')
-    parser.add_argument('--mjpg', \
-        action='store_true', \
-        help='If set, capture video in motion jpeg format')
+    parser = argparse.ArgumentParser(description='TRT Pose Demo')
+    ContinuousVideoProcess.prepareArguments(parser)
     args = parser.parse_args()
+    # Create continuous video process and start it
+    vproc = PoseEstimationProcess(args)
+    vproc.execute()
     
-    fpsCounter = IntervalCounter(10)
-    
-    model = PoseCaptureModel( \
-        WIDTH, HEIGHT, MODEL_WEIGHTS, OPTIMIZED_MODEL, TASK_DESC)
-    
-    fourcc = None
-    if args.mjpg:
-        fourcc = 'MJPG'
-    capture = ContinuousVideoCapture( \
-        args.camera, args.width, args.height, args.fps, args.qsize, fourcc) 
-  
-    colorConv = ColorConvert(args.qsize, capture)
-    resize = Resize(args.qsize, colorConv)
-    preprocess = Preprocess(args.qsize, resize, model)    
-    inference = Inference(args.qsize, preprocess, model)  
-    postprocess = Postprocess(args.qsize, inference, model)
-    
-    postprocess.start()
-    inference.start()
-    preprocess.start()
-    resize.start()
-    colorConv.start()
-    capture.start()
-    
-    while True:
-    
-        frame = postprocess.get()
-        
-        # Debug
-        if args.qinfo:
-            print('%02d %06d - %02d %06d - %02d %06d - %02d %06d - %02d %06d - %02d %06d' % ( \
-                capture.qsize(), capture.numDrops, \
-                resize.qsize(), resize.numDrops, \
-                colorConv.qsize(), colorConv.numDrops, \
-                preprocess.qsize(), preprocess.numDrops, \
-                inference.qsize(), inference.numDrops, \
-                postprocess.qsize(), postprocess.numDrops
-                ))
-        
-        interval = fpsCounter.measure()
-        if interval is not None:
-            fps = 1.0 / interval
-            fpsInfo = '{0}{1:.2f}   ESC to Quit'.format('FPS:', fps)
-            cv2.putText(frame, fpsInfo, (32, 32), \
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
-        
-        cv2.imshow(WINDOW_TITLE, frame)
-        
-        # Check if ESC key is pressed to terminate this application
-        key = cv2.waitKey(1)
-        if key == 27: # ESC
-            break
-            
-    postprocess.stop()
-    inference.stop()
-    preprocess.stop()
-    resize.stop()
-    colorConv.stop()
-    capture.stop()
 
 if __name__ == '__main__':
     main() 
--- video_app_utils.py
+++ video_app_utils.py
@@ -33,6 +33,7 @@ import threading
 import cv2
 import time
 import numpy as np
+import argparse
 
 
 class PipelineWorker():
@@ -48,6 +49,8 @@ class PipelineWorker():
     Attributes:
         queue: Queue to store outputs processed by this instance.
         source: Data source (assumped to be other PipelineWorker instance) 
+        destination: Data destination
+                     (assumped to be other PipelineWorker instance)  
         sem: Semaphore to lock this instance.
         flag: If ture, the processing loop is running.
         numDrops: Total number of dropped outputs.
@@ -63,12 +66,18 @@ class PipelineWorker():
         '''
         self.queue = queue.Queue(qsize)
         self.source = source
+        if self.source is not None:
+            self.source.destination = self
+        self.destination = None
         self.sem = threading.Semaphore(1)
         self.flag = False
         self.numDrops = 0
     
     def __del__(self):
         pass
+
+    def __repr__(self):
+        return '%02d %06d' % (self.qsize(), self.numDrops)
         
     def process(self, srcData):
         '''Data processing(producing) method called in thread loop.
@@ -140,7 +149,7 @@ class ContinuousVideoCapture(PipelineWorker):
     ! videoconvert \
     ! appsink'
     
-    def __init__(self, cameraId, width, height, fps=30, qsize=30, fourcc=None):
+    def __init__(self, cameraId, width, height, fps=None, qsize=30, fourcc=None):
         '''
             Args:
                 cameraId(int): Camera device ID, if negative number specified,
@@ -174,7 +183,8 @@ class ContinuousVideoCapture(PipelineWorker):
             if fourcc is not None:
                 self.capture.set(cv2.CAP_PROP_FOURCC, \
                     cv2.VideoWriter_fourcc(*fourcc))
-            self.capture.set(cv2.CAP_PROP_FPS, fps)
+            if fps is not None:
+                self.capture.set(cv2.CAP_PROP_FPS, fps)
         
          # Get the actual frame size
         self.width = self.capture.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -233,4 +243,114 @@ class IntervalCounter():
             return np.average(self.samples)
         else:
             return None
+
+
+class ContinuousVideoProcess():
+    '''Captured video processing applicaion framework
+    
+    Attributes:
+        capture(ContinuousVideoCapture): Video capture process
+        fpsCounter(IntervalCounter): FPS counter
+        qinfo(bool): If set, print processing queue status
+        title(str): Window title
+        pipeline(list): List of the pipeline worker objects
+    '''
+    
+    def __init__(self, args):
+        '''
+        Args:
+            args(argparse.Namespace): video capture command-line arguments
+        '''
+        fourcc = None
+        if args.mjpg:
+            fourcc = 'MJPG'
+        if args.fps < 0:
+            fps = None
+        else:
+            fps = args.fps
+        self.capture = ContinuousVideoCapture( \
+            args.camera, args.width, args.height, fps, args.qsize, fourcc)
+        self.fpsCounter = IntervalCounter(10)
+        self.qinfo = args.qinfo
+        self.title = args.title
+        self.pipeline = None
+
+    def scanPipeline(self):
+        pipeline = []
+        self.__class__.getSources(self.capture, pipeline)
+        self.pipeline = pipeline[::-1]
+
+    def startPipeline(self):
+        self.scanPipeline()
+        for worker in self.pipeline:
+            worker.start()
+
+    def stopPipeline(self):
+        for worker in self.pipeline:
+            worker.stop()
+
+    def execute(self):
+        ''' execute video processing loop
+        '''
+        self.startPipeline()
+        while True:
+            frame = self.getOutput()
+            if self.qinfo:
+                print(self.pipeline)
+            interval = self.fpsCounter.measure()
+            if interval is not None:
+                fps = 1.0 / interval
+                fpsInfo = '{0}{1:.2f}   ESC to Quit'.format('FPS:', fps)
+                cv2.putText(frame, fpsInfo, (32, 32), \
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
+            cv2.imshow(self.title, frame)  
+            # Check if ESC key is pressed to terminate this application
+            key = cv2.waitKey(1)
+            if key == 27: # ESC
+                break
+        self.stopPipeline()
+
+    def getOutput(self):
+        ''' Get the output image to be displayed.
+
+        Returns:
+            Output image
+        '''
+        frame = (self.pipeline[0]).get()
+        return frame
+
+    @staticmethod
+    def getSources(worker, srcList):
+        if worker is None:
+            return
+        else:
+            srcList.append(worker)
+        ContinuousVideoProcess.getSources(worker.destination, srcList)
+
+    @staticmethod
+    def prepareArguments(parser, width=640, height=480):
+        parser.add_argument('--camera', '-c', \
+            type=int, default=0, metavar='CAMERA_NUM', \
+            help='Camera number, use any negative integer for MIPI-CSI camera')
+        parser.add_argument('--width', \
+            type=int, default=width, metavar='WIDTH', \
+            help='Capture width')
+        parser.add_argument('--height', \
+            type=int, default=height, metavar='HEIGHT', \
+            help='Capture height')
+        parser.add_argument('--fps', \
+            type=int, default=-1, metavar='FPS', \
+            help='Capture frame rate')
+        parser.add_argument('--qsize', \
+            type=int, default=1, metavar='QSIZE', \
+            help='Capture queue size')
+        parser.add_argument('--qinfo', \
+            action='store_true', \
+            help='If set, print queue status information')
+        parser.add_argument('--mjpg', \
+            action='store_true', \
+            help='If set, capture video in motion jpeg format')
+        parser.add_argument('--title', \
+            type=str, default=parser.prog, metavar='TITLE', \
+            help='Display window title')
 
